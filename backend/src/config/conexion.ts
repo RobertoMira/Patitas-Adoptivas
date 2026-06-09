b679d9f3e9cef3b2a02468896352332cb94ab60e
RobertoMira <roberto.miramontes0605@gmail.com>
2026-06-09 13:38:01 -0700

    chore: update dependencies

--- backend/src/config/conexion.ts
+++ backend/src/config/conexion.ts
@@ -3,6 +3,6 @@ import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
     host: 'localhost',
     user: 'root', // Cambia por tu usuario
-    password: 'MyMirai123', // Cambia por tu password
+    password: '123456789', // Cambia por tu password
     database: 'patitas_db'
 });
